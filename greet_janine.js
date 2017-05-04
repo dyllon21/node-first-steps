a965024d1c74182cc5f4943590a2123f0d9c0ff2
Dyllon <isaacsdyllon123@gmail.com>
2017-05-04 16:01:25 +0200

    made use of figlet styling

--- greet_janine.js
+++ greet_janine.js
@@ -1,10 +1,35 @@
+
 //To separate functions and the code calling them into separate files you use modules. That allow functions to be reused, as they can be called from various different files.
 
-const greet = require('./greet');
+// const greet = require('./greet');
+//
+// module.exports = function(name) {
+//   return "hello, " + name;
+// }
+//
+// console.log(greet('Janine'));
+//Run it using NodeJS, node greet_janine.js. You should see the same output as before.
+
+//styling greeting
+const chalk = require('chalk');
 
 module.exports = function(name) {
-  return "hello, " + name;
-}
+   return "hello, " + name;
+ }
+ const greet = require('./greet');
 
-console.log(greet('Janine'));
-//Run it using NodeJS, node greet_janine.js. You should see the same output as before.
+ const styledMessage = chalk.bgBlue.black(greet("Janine"));
+ console.log(styledMessage);
+
+//using figlet:
+
+var figlet = require('figlet');
+
+figlet('Hello Janine!!', function (err, data) {
+  if (err) {
+    console.log('Something went wrong...');
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
